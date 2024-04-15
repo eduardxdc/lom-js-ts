f3b9507d0e7eb8bd9ebd1f7a43b9be49b114d3fb
Eduardo <141167453+eduardxdc@users.noreply.github.com>
2024-04-14 22:40:56 -0300

    comments

---
+++ module01/index.js
@@ -0,0 +1,8 @@
+// for comments we use double slashes
+// comments are ignored by the javascript engine, making it useful for debug testing
+
+/* for block comments,
+ we use these signals  */
+
+console.log("testing comments js"); // line executed by javascript
+// console.log("testing comments"); <-- ignored
