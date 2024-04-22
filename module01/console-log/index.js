96f3e247e8c4da9e8296d811346c814c8ba2df36
Eduardo <141167453+eduardxdc@users.noreply.github.com>
2024-04-21 23:33:05 -0300

    console.log

--- module01/console-log/index.js
+++ module01/console-log/index.js
@@ -1,5 +1,5 @@
-console.log("double quotes"); // string= text;
-console.log('single quotes'); // string= text;
-console.log(`crasis`); // string= text;
+console.log("double quotes"); // string = text;
+console.log('single quotes'); // string = text;
+console.log(`crasis`); // string = text;
 console.log(77); // number = int;
 console.log(77.77); // number = float/double;
